8b1154c6e261a0bf16a22af75fcbb13d4c9bb744
James F. Thomas <jthomas@rsimail.com>
2023-02-20 08:48:59 -0600

    NextJS project + MUI + dependencies set up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+/* eslint-disable @next/next/no-page-custom-font */
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  <Head>
+    <title> Next.js Practice</title>
+    <meta
+      name="viewport"
+      content="minimum-scale=1, initial-scale=1, width=device-width"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    />
+  </Head>;
+  return <Component {...pageProps} />;
+};
+
+export default App;
