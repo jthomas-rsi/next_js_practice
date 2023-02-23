15bd88b63376f6ca421d681658f3f783f5762cc0
James F. Thomas <jthomas@rsimail.com>
2023-02-23 10:31:46 -0600

    basic styling done Header & Footer comps

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,24 +8,35 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import { useState } from "react";
+import { Paper } from "@mui/material";
 
 const Footer = () => {
   const [value, setValue] = useState(0);
 
   return (
-    <Box sx={{ width: 500 }}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction label="LinkedIn" icon={<LinkedInIcon />} />
-        <BottomNavigationAction label="Facebook" icon={<FacebookIcon />} />
-        <BottomNavigationAction label="Twitter" icon={<TwitterIcon />} />
-      </BottomNavigation>
-    </Box>
+    <Paper
+      sx={{
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        right: 0,
+      }}
+    >
+      <Box sx={{ width: "100%" }}>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+          sx={{ backgroundColor: "#D5BDAF" }}
+        >
+          <BottomNavigationAction label="LinkedIn" icon={<LinkedInIcon />} />
+          <BottomNavigationAction label="Facebook" icon={<FacebookIcon />} />
+          <BottomNavigationAction label="Twitter" icon={<TwitterIcon />} />
+        </BottomNavigation>
+      </Box>
+    </Paper>
   );
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,24 +4,20 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import Avatar from "@mui/material/Avatar";
 
 const Header = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar
+        position="static"
+        component="nav"
+        sx={{ backgroundColor: "#D5BDAF" }}
+      >
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+          <Avatar sx={{ mr: 1, backgroundColor: "#D6CCC2" }}>WP</Avatar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Work Project
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
