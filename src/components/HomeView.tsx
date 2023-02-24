b3eba7857ac56b522fd152ac5121dc7baf46b65a
James F. Thomas <jthomas@rsimail.com>
2023-02-24 13:43:06 -0600

    HomeView styles to match online services repo

--- src/components/HomeView.tsx
+++ src/components/HomeView.tsx
@@ -1,19 +1,54 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Grid, Icon, Typography } from "@mui/material";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
 
 const HomeView = () => {
   return (
-    <Box
+    <Grid
+      container
       sx={{
-        minHeight: "100vh",
+        height: "90vh",
         backgroundColor: "#F5EBE0",
         display: "flex",
         justifyContent: "center",
       }}
     >
-      <Typography variant="h6" mt={3}>
-        Home View Component
-      </Typography>
-    </Box>
+      <Box mt={3}>
+        <Typography variant="h6" sx={{ mb: 3 }} textAlign={"center"}>
+          {`Welcome To ABC's Online Service Portal`}
+        </Typography>
+        <Typography variant="h4" textAlign={"center"}>
+          How Can We Help You?
+        </Typography>
+        <Box
+          sx={{
+            backgroundColor: "white",
+            display: "flex",
+            flexDirection: "column",
+            // justifyContent: "center",
+            alignItems: "center",
+          }}
+          height={150}
+          width={400}
+          mt={4}
+        >
+          <Icon
+            color="primary"
+            sx={{
+              fontSize: "40px !important",
+              marginBottom: 4,
+            }}
+          >
+            <MenuBookIcon />
+          </Icon>
+          <Button
+            variant="contained"
+            sx={{ height: "fit-content", width: "fit-content" }}
+          >
+            Customer Services
+          </Button>{" "}
+        </Box>
+      </Box>
+    </Grid>
   );
 };
 
